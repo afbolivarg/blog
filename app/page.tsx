02cdad903d2bfa7879bdea02bf18d1d4e00bd6cf
Andrés Bolívar <afbolivarg@gmail.com>
2024-09-14 15:54:51 -0500

    resume

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,13 @@ export default function Page() {
         {`Working on the intersection of technology and business.`}
       </p>
       <p className="mb-4">
-        {`Colombian self-taught developer with experience as a fullstack developer and ML engineer,
-        currently wrapping up a Business Administration degree on a full scholarship. Passionate about technology, business, and design, 
-        with a strong focus on creating functional, beautiful, and valuable solutions that drive 
-        business success.`}
+        {`I'm Colombian self-taught developer with experience as a full-stack developer and ML engineer,
+        currently wrapping up a Business Administration degree on a full scholarship. I'm passionate about technology, business, and design. 
+        I love creating functional, beautiful, and valuable solutions that drive business success.`}
+      </p>
+      <p className="mb-4">
+        {`In addition to my studies and my day job, I'm working on a few projects and writing about them here, as well as sharing my 
+        thoughts on books, business, and technology.`}
       </p>
       <div className="my-8">
         <BlogPosts />
