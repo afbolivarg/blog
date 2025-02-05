c2d3f6514bac8541ea77b345953306ba61317731
Andrés Bolívar <afbolivarg@gmail.com>
2025-02-05 11:34:09 -0500

    minor overhaul

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -18,15 +18,15 @@ export function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="flex flex-col space-y-1 mb-4 hover:underline decoration-neutral-500"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
+            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 justify-between">
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
                 {post.metadata.title}
+              </p>
+              <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
+                {formatDate(post.metadata.publishedAt, false)}
               </p>
             </div>
           </Link>
